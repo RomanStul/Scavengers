bd82dbecd18d18064ee37373b5d559567dbcf939
RomanStul <stuliro1@fel.cvut.cz>
2025-03-22 14:05:34 +0100

    fuel consumption

--- Assets/Scripts/Player/Module/Movement/Movement.cs
+++ Assets/Scripts/Player/Module/Movement/Movement.cs
@@ -13,12 +13,16 @@ namespace Player.Module.Movement
             public float Thrust;
             public float RotationThrust;
             public float MaxSpeed;
+            public float fuelPerSecond;
+            public float MaxFuel;
         }
         
         //================================================================EDITOR VARIABLES
         [SerializeField] protected MovementConstants movementVariables;
         
         [SerializeField] protected GameObject moduleBody;
+
+        [SerializeField] protected float currentFuel;
         //================================================================GETTER SETTER
         public float ThrustInput { get; set; } = 0f;
         public float RotationInput { get; set; } = 0f;
@@ -31,13 +35,25 @@ namespace Player.Module.Movement
         //================================================================FUNCTIONALITY
 
         protected Rigidbody2D Rigid, RotationRigid;
-        
+
+        private void Awake()
+        {
+            Refuel();
+        }
+
+
         private void Update()
         {
             //TODO add fuel consumption functionality
+            Debug.Log(currentFuel);
+            if (currentFuel <= 0)
+            {
+                return;
+            }
             
             if (ThrustInput > 0 || ModuleRef.GetScript<Upgrades.Upgrades>(Module.ScriptNames.UpgradesScript).IsActive(Upgrades.Upgrades.Ups.Reverse) && ThrustInput < 0)
             {
+                currentFuel -= Mathf.Abs(ThrustInput) * movementVariables.fuelPerSecond * Time.deltaTime;
                 Rigid.AddForce(moduleBody.transform.up * (ThrustInput * movementVariables.Thrust));
                 float speed = Rigid.velocity.magnitude;
                 if (speed > movementVariables.MaxSpeed)
@@ -48,8 +64,22 @@ namespace Player.Module.Movement
 
             if (Mathf.Abs(RotationInput) > 0)
             {
+                currentFuel -= Mathf.Abs(RotationInput) *movementVariables.fuelPerSecond * Time.deltaTime;
                 RotationRigid.AddTorque(-RotationInput * movementVariables.RotationThrust);
             }
+        }
+
+        public void Refuel(float amount = -1)
+        {
+            if (amount < 0)
+            {
+                currentFuel = movementVariables.MaxFuel;
+            }
+            else
+            {
+                currentFuel += amount;
+            }
+            
         }
     }
 }
